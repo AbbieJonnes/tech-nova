7a479b525b1b6909cbfc9533d4706911993f5c5f
abbiejonnes <abigaelmwangi534@gmail.com>
2026-05-18 16:14:55 +0300

    update:update data on all files

---
+++ script.js
@@ -0,0 +1,16 @@
+const toggle = document.getElementById("themeToggle");
+const icon = toggle.querySelector("i");
+
+toggle.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+    } else {
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+    }
+});
